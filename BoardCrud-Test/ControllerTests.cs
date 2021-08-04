540f0369e45db62c02fbc2f81cc63e0e6ccab45f
Catherine Cheatle <48526094+clcheatle@users.noreply.github.com>
2021-08-04 10:22:46 -0400

    Added Skeleton

---
+++ BoardCrud-Test/ControllerTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BoardCrud_Test
+{
+    [TestClass]
+    public class ControllerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ BoardCrud/Controllers/BoardController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BoardCrud.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BoardController : ControllerBase
+    {
+
+        private readonly ILogger<BoardController> _logger;
+
+        public BoardController(ILogger<BoardController> logger)
+        {
+            _logger = logger;
+        }
+
+        
+    }
+}
